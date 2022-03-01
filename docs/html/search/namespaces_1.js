db1a255bd38a91d7eb9d861546d6cb61e248fde7
Imdimark <78663960+Imdimark@users.noreply.github.com>
2022-03-01 23:36:40 +0000

    Add files via upload

---
+++ docs/html/search/namespaces_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['user_5finterface_15',['user_interface',['../namespaceuser__interface.html',1,'']]]
+];
---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "cdfgmnprsu",
+  1: "gu",
+  2: "ps",
+  3: "cdfgmnru"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "namespaces",
+  2: "files",
+  3: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Namespaces",
+  2: "Files",
+  3: "Functions"
+};
+
